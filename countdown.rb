6631682cc48e5a12ffcebbe9f02c977643909f6d
kamausuzan197 <kamaususan197@gmail.com>
2022-08-16 08:39:42 +0300

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (interger)
+    while interger > 0
+        puts"#{interger} SECOND(S)!"
+    interger -=1
 end
+"HAPPY NEW YEAR!"
+end
+
+def contdown_with_sleep (interger)
+    while interger > 0
+        puts "#{interger} SECOND(5)!"
+    interger -=1
+    sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end 
